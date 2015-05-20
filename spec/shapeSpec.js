a6eb6b8ff649f55f3fd737760a4e4ea4c34ea241
415DomSmith <415.dominic.smith@gmail.com>
2015-05-20 16:21:45 -0700

    turned on all tests

--- spec/shapeSpec.js
+++ spec/shapeSpec.js
@@ -11,25 +11,25 @@ describe("Shape", function() {
   });
 
   describe("Area", function() {
-    xit("should should be 0 for this abstract shape", function() {
+    it("should should be 0 for this abstract shape", function() {
       expect(shape.area()).toEqual(0);
     });
   });
 
   describe("Draw", function(){
-  	xit("should describe a shape with the passed number of sides", function() {
+  	it("should describe a shape with the passed number of sides", function() {
   		expect(shape.draw()).toEqual("A shape with 1 sides");
   	});
   });
 
   describe("toString", function(){
-  	xit("should report the number of side and the color of the shape as a string", function(){
+  	it("should report the number of side and the color of the shape as a string", function(){
   		expect(shape.toString()).toEqual("[Shape sides:1, color:red ]")
   	});
   });
   
   describe("getRGB", function(){
-  	xit("should return the rgb value of the color of the shape, as a string", function(){
+  	it("should return the rgb value of the color of the shape, as a string", function(){
   		expect(shape.getRGB()).toEqual("rgb(255,0,0)")
   	});	
   });
--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -8,25 +8,25 @@ describe("Square", function() {
   });
 
   describe("Area", function() {
-    xit("should be 4 for a square", function() {
+    it("should be 4 for a square", function() {
       expect(square.area()).toEqual(4);
     });
   });
 
   describe("Perimeter", function() {
-    xit("should be 8 for a square with side length 2", function() {
+    it("should be 8 for a square with side length 2", function() {
       expect(square.perimeter()).toEqual(8);
     });
   });
 
   describe("Draw", function(){
-    xit("should describe a shape with 4 sides", function(){
+    it("should describe a shape with 4 sides", function(){
       expect(square.draw()).toEqual("A shape with 4 sides");
     });
   });
 
   describe("toString", function(){
-    xit("should report the number of sides and the color of the shape as a string", function(){
+    it("should report the number of sides and the color of the shape as a string", function(){
       expect(square.toString()).toEqual("[Shape sides:4, color:red ]")
     });
   });
